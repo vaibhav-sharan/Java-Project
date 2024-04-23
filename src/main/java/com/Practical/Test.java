5b6c59604c7393aababa811249f6c8310b2c0b0f
vaibhav-sharan <vaibhavsharan1617@gmail.com>
2024-04-23 17:20:18 +0530

    refresh

--- src/main/java/com/Practical/Test.java
+++ src/main/java/com/Practical/Test.java
@@ -2,13 +2,7 @@ package com.Practical;
 
 public class Test {
   public static void main(String[] args) {
-    System.out.println("Hello");
     System.out.println("Hello1");
-    System.out.println("Hello2");
-    System.out.println("Hello3");
-    System.out.println("Hello4");
-    System.out.println("Hello5");
-    System.out.println("Hello6");
-    System.out.println("Hello7");
+
   }
   }
